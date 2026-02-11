79dadf2deb296eb7a82d87dd2cd02a09608829a3
Miko-29 <muskan2912002@gmail.com>
2026-02-11 22:24:24 +0530

    favicon

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Pomodoro/pomodoro-timer/', // Repo name only
+  base: '/Pomodoro/', // Repo name only
 })
